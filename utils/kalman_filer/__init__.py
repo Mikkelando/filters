567b8b2b6795a752425397bc74d856552f359658
Mikkelando <ms.m.kl@mail.ru>
2024-07-19 04:13:16 +0300

    minor upd

--- utils/kalman_filer/__init__.py
+++ utils/kalman_filer/__init__.py
@@ -1,1 +1,2 @@
-from .kalman_utils import klmn_filter, stream_klmn_filter, get_filters
+from .kalman_utils import stream_klmn_filter, get_filters
+from .kalman_utils import klmn_filter_parallel as klmn_filter
