14d78e6baf4af1662a6f6638bdf7bc99ae420580
Mikkelando <ms.m.kl@mail.ru>
2024-08-02 14:27:37 +0300

    migrated to skimage v0.18

--- utils/euro_filter/filter_utils.py
+++ utils/euro_filter/filter_utils.py
@@ -1,12 +1,13 @@
 import csv
 import subprocess
+import functools
 import time
 import cv2
 import os
 import numpy as np
-from skimage.exposure import match_histograms
+# from skimage.exposure import match_histograms
 
-from skimage._shared import utils 
+# from skimage._shared import utils 
 import shutil
 import mediapipe as mp
 
@@ -39,10 +40,10 @@ def blur_area(image, ksize=(7, 7), sigmaX=0):
 
 
 
-def histogram_matching(source, driving):
+# def histogram_matching(source, driving):
     
     
-    return match_histograms(source, driving, channel_axis=-1)
+#     return match_histograms(source, driving, channel_axis=-1)
 
 
 
@@ -82,10 +83,125 @@ def seamless_clone(original_image, generated_image, mask=None, center=None, meth
 
 
 
+class channel_as_last_axis:
+    """Decorator for automatically making channels axis last for all arrays.
 
+    This decorator reorders axes for compatibility with functions that only
+    support channels along the last axis. After the function call is complete
+    the channels axis is restored back to its original position.
 
+    Parameters
+    ----------
+    channel_arg_positions : tuple of int, optional
+        Positional arguments at the positions specified in this tuple are
+        assumed to be multichannel arrays. The default is to assume only the
+        first argument to the function is a multichannel array.
+    channel_kwarg_names : tuple of str, optional
+        A tuple containing the names of any keyword arguments corresponding to
+        multichannel arrays.
+    multichannel_output : bool, optional
+        A boolean that should be True if the output of the function is not a
+        multichannel array and False otherwise. This decorator does not
+        currently support the general case of functions with multiple outputs
+        where some or all are multichannel.
 
+    """
 
+    def __init__(
+        self,
+        channel_arg_positions=(0,),
+        channel_kwarg_names=(),
+        multichannel_output=True,
+    ):
+        self.arg_positions = set(channel_arg_positions)
+        self.kwarg_names = set(channel_kwarg_names)
+        self.multichannel_output = multichannel_output
+
+    def __call__(self, func):
+        @functools.wraps(func)
+        def fixed_func(*args, **kwargs):
+            channel_axis = kwargs.get('channel_axis', None)
+
+            if channel_axis is None:
+                return func(*args, **kwargs)
+
+            # TODO: convert scalars to a tuple in anticipation of eventually
+            #       supporting a tuple of channel axes. Right now, only an
+            #       integer or a single-element tuple is supported, though.
+            if np.isscalar(channel_axis):
+                channel_axis = (channel_axis,)
+            if len(channel_axis) > 1:
+                raise ValueError("only a single channel axis is currently supported")
+
+            if channel_axis == (-1,) or channel_axis == -1:
+                return func(*args, **kwargs)
+
+            if self.arg_positions:
+                new_args = []
+                for pos, arg in enumerate(args):
+                    if pos in self.arg_positions:
+                        new_args.append(np.moveaxis(arg, channel_axis[0], -1))
+                    else:
+                        new_args.append(arg)
+                new_args = tuple(new_args)
+            else:
+                new_args = args
+
+            for name in self.kwarg_names:
+                kwargs[name] = np.moveaxis(kwargs[name], channel_axis[0], -1)
+
+            # now that we have moved the channels axis to the last position,
+            # change the channel_axis argument to -1
+            kwargs["channel_axis"] = -1
+
+            # Call the function with the fixed arguments
+            out = func(*new_args, **kwargs)
+            if self.multichannel_output:
+                out = np.moveaxis(out, -1, channel_axis[0])
+            return out
+
+        return fixed_func
+new_float_type = {
+    # preserved types
+    np.float32().dtype.char: np.float32,
+    np.float64().dtype.char: np.float64,
+    np.complex64().dtype.char: np.complex64,
+    np.complex128().dtype.char: np.complex128,
+    # altered types
+    np.float16().dtype.char: np.float32,
+    'g': np.float64,  # np.float128 ; doesn't exist on windows
+    'G': np.complex128,  # np.complex256 ; doesn't exist on windows
+}
+
+def _supported_float_type(input_dtype, allow_complex=False):
+    """Return an appropriate floating-point dtype for a given dtype.
+
+    float32, float64, complex64, complex128 are preserved.
+    float16 is promoted to float32.
+    complex256 is demoted to complex128.
+    Other types are cast to float64.
+
+    Parameters
+    ----------
+    input_dtype : np.dtype or tuple of np.dtype
+        The input dtype. If a tuple of multiple dtypes is provided, each
+        dtype is first converted to a supported floating point type and the
+        final dtype is then determined by applying `np.result_type` on the
+        sequence of supported floating point types.
+    allow_complex : bool, optional
+        If False, raise a ValueError on complex-valued inputs.
+
+    Returns
+    -------
+    float_type : dtype
+        Floating-point dtype for the image.
+    """
+    if isinstance(input_dtype, tuple):
+        return np.result_type(*(_supported_float_type(d) for d in input_dtype))
+    input_dtype = np.dtype(input_dtype)
+    if not allow_complex and input_dtype.kind == 'c':
+        raise ValueError("complex valued input is not supported")
+    return new_float_type.get(input_dtype.char, np.float64)
 
 
 def _match_cumulative_cdf(source, template, strength=1.0):
@@ -126,7 +242,7 @@ def _match_cumulative_cdf(source, template, strength=1.0):
     matched_values = (1 - strength) * source.reshape(-1) + strength * interp_a_values[src_lookup]
     return matched_values.reshape(source.shape)
 
-@utils.channel_as_last_axis(channel_arg_positions=(0, 1))
+@channel_as_last_axis(channel_arg_positions=(0, 1))
 def match_histograms_moded(image, reference, *, strength=1.0, channel_axis=-1):
     """Adjust an image so that its cumulative histogram matches that of another.
 
@@ -186,7 +302,7 @@ def match_histograms_moded(image, reference, *, strength=1.0, channel_axis=-1):
 
     if matched.dtype.kind == 'f':
         # output a float32 result when the input is float16 or float32
-        out_dtype = utils._supported_float_type(image.dtype)
+        out_dtype = _supported_float_type(image.dtype)
         matched = matched.astype(out_dtype, copy=False)
     return matched
 
